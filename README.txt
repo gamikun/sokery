e8fbb059dd00baed98a5dd2a866de2da99e7b4ff
Gamaliel Espinoza <gamaliel.espinoza@gmail.com>
2017-06-11 14:11:21 -0700

    Fixed index.html access.

--- README.txt
+++ README.txt
@@ -1,5 +1,6 @@
 CHANGEL
 
-Version 0.1.1
+Version 0.1.2
 =============
 - Autoimplemented module, running in port 4991.
+- Fixed index.html access.
--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
   version = sokery.__version__,
   description = 'Socket testing with web interface',
   install_requires=['tornado'],
+  package_data = {'sokery': ['web/views/index.html']},
   author = 'Gamaliel Espinoza',
   author_email = 'gamaliel.espinoza@gmail.com',
   url = 'https://github.com/gamikun/sokery',
