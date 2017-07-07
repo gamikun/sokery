0cc41d84dc4f1b2bfe9dda4e78656705917430fc
Alma García <agarcia@edesarrollos.com>
2017-07-06 23:41:11 -0700

    New preopening ports feature, it can be opened from cli with no further intervention.

--- sokery/loop.py
+++ sokery/loop.py
@@ -1,10 +1,9 @@
 from os.path import join, dirname
-from tornado.web import Application
+from web.app import Application
 from tornado.ioloop import IOLoop
 from web.admin import LiveHandler,\
 					  StaticFileHandler
 from argparse import ArgumentParser
-
 
 
 basepath = dirname(__file__)
@@ -17,11 +16,28 @@ urlmap = [
 	(r'/()$', StaticFileHandler, {'path': indexpath}),
 ]
 
-def start_loop():
-	app = Application(urlmap, report_clients=[])
-	app.listen(4991)
+def start_loop(port=4991, listen_to=None):
+	app = Application(urlmap,
+		report_clients=[],
+		listen_to=listen_to,
+		)
+	app.listen(port)
 	loop = IOLoop.current()
 	loop.start()
 
 if __name__ == '__main__':
-	start_loop()
+	from argparse import ArgumentParser
+	from utils import portlist
+
+	parser = ArgumentParser()
+	parser.add_argument('-p', dest='port', default=4991, type=int)
+	parser.add_argument('-l', dest='listen', type=portlist)
+
+	args = parser.parse_args()
+
+	try:
+		start_loop(port=args.port,
+				   listen_to=args.listen
+				   )
+	except KeyboardInterrupt:
+		print('end of loop')
