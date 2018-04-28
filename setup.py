0a383df148bff662c722899f6a1b3f331b069eb1
Gamaliel Espinoza <gamaliel.espinoza@gmail.com>
2018-04-28 01:08:22 -0700

    sokery binary in setup

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(
   version = sokery.__version__,
   description = 'Socket testing with web interface',
   install_requires=['tornado'],
+  scripts = [join(binpath, 'sokery')],
   package_data = {
     'sokery': ['web/views/index.html', 'web/views/ng.js']
   },
